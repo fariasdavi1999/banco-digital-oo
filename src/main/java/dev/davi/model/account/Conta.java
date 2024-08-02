4b1b23f1f751d12730611060a0f522f1c827513f
fariasdavi1999 <fariasdavi1999@gmail.com>
2024-08-02 15:26:13 -0300

    refactor: refatorando metodos, adicionando atributo de tipo de conta

--- src/main/java/dev/davi/model/account/Conta.java
+++ src/main/java/dev/davi/model/account/Conta.java
@@ -8,10 +8,11 @@ public abstract class Conta implements IConta {
 	private static final int AGENCIA_PADRAO = 1;
 	private static int sequencial = 1;
 
+	protected Cliente cliente;
+	protected TipoConta tipoConta;
 	protected int agencia;
 	protected int numero;
 	protected double saldo;
-	protected Cliente cliente;
 
 	protected Conta(Cliente cliente) {
 		this.agencia = Conta.AGENCIA_PADRAO;
@@ -21,6 +22,7 @@ public abstract class Conta implements IConta {
 
 	protected void imprimirInfosComuns() {
 		System.out.println("Titular: " + this.cliente.getNome());
+		System.out.println("Conta: " + this.tipoConta.getDescricao());
 		System.out.println("Agencia: " + this.agencia);
 		System.out.println("Numero: " + this.numero);
 		System.out.println("Saldo: " + this.saldo);
@@ -38,19 +40,37 @@ public abstract class Conta implements IConta {
 		return saldo;
 	}
 
-	@Override
-	public void sacar(double valor) {
-		saldo -= valor;
+	public TipoConta getTipoConta() {
+		return tipoConta;
+	}
+
+	public void setTipoConta(TipoConta tipoConta) {
+		this.tipoConta = tipoConta;
 	}
 
 	@Override
-	public void depositar(double valor) {
-		saldo += valor;
+	public void sacar(double valor, IConta contaOrigem) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			System.out.println("Novo Saldo: " + this.saldo);
+		} else {
+			System.out.println("Saldo insuficiente.");
+		}
+	}
+
+	@Override
+	public void depositar(double valor, IConta contaDestino) {
+		this.saldo += valor;
 	}
 
 	@Override
 	public void transferir(double valor, IConta contaDestino) {
-		this.sacar(valor);
-		contaDestino.depositar(valor);
+		if (this.saldo >= valor) {
+			this.sacar(valor, this);
+			contaDestino.depositar(valor, this);
+		} else {
+			System.out.println("Saldo insuficiente para tranferir.");
+		}
+
 	}
 }
