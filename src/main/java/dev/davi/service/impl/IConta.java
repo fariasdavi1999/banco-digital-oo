fc80ccf1633b535fe4eb7ed7219815b10696004e
fariasdavi1999 <fariasdavi1999@gmail.com>
2024-08-02 15:24:53 -0300

    refactor: adicionando parametros de contas de origem e destino nos metodos para impl

--- src/main/java/dev/davi/service/impl/IConta.java
+++ src/main/java/dev/davi/service/impl/IConta.java
@@ -1,12 +1,12 @@
 package src.main.java.dev.davi.service.impl;
 
 public interface IConta {
-	
-	void sacar(double valor);
-	
-	void depositar(double valor);
-	
+
+	void sacar(double valor, IConta contaOrigem);
+
+	void depositar(double valor, IConta contaDestino);
+
 	void transferir(double valor, IConta contaDestino);
-	
+
 	void imprimirExtrato();
 }
