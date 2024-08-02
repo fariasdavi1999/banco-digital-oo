79fa60a4dfaf7c487bb4919a80787049344b35fa
fariasdavi1999 <fariasdavi1999@gmail.com>
2024-08-02 15:25:26 -0300

    refactor: setando tipo de conta no construtor da classe

--- src/main/java/dev/davi/accounts/ContaCorrente.java
+++ src/main/java/dev/davi/accounts/ContaCorrente.java
@@ -1,17 +1,19 @@
 package src.main.java.dev.davi.accounts;
 
-import src.main.java.dev.davi.model.client.Cliente;
 import src.main.java.dev.davi.model.account.Conta;
+import src.main.java.dev.davi.model.account.TipoConta;
+import src.main.java.dev.davi.model.client.Cliente;
 
 public class ContaCorrente extends Conta {
 
 	public ContaCorrente(Cliente cliente) {
 		super(cliente);
+		setTipoConta(TipoConta.CONTA_CORRENTE);
 	}
 
 	@Override
 	public void imprimirExtrato() {
-		System.out.println("=== Extrato Conta Corrente ===");
+		System.out.println("=== Extrato " + tipoConta.getDescricao() + " ===");
 		super.imprimirInfosComuns();
 	}
 }
--- src/main/java/dev/davi/accounts/ContaPoupanca.java
+++ src/main/java/dev/davi/accounts/ContaPoupanca.java
@@ -1,17 +1,19 @@
 package src.main.java.dev.davi.accounts;
 
-import src.main.java.dev.davi.model.client.Cliente;
 import src.main.java.dev.davi.model.account.Conta;
+import src.main.java.dev.davi.model.account.TipoConta;
+import src.main.java.dev.davi.model.client.Cliente;
 
 public class ContaPoupanca extends Conta {
 
 	public ContaPoupanca(Cliente cliente) {
 		super(cliente);
+		setTipoConta(TipoConta.CONTA_POUPANCA);
 	}
 
 	@Override
 	public void imprimirExtrato() {
-		System.out.println("=== Extrato Conta Poupanca ===");
+		System.out.println("=== Extrato " + tipoConta.getDescricao() + " ===");
 		super.imprimirInfosComuns();
 	}
 }
