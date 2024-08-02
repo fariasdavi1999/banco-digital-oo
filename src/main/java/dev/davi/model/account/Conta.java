0eeb5f62235fd314fbf844ea824aadf696694da6
fariasdavi1999 <fariasdavi1999@gmail.com>
2024-08-02 13:50:54 -0300

    refactor: mudando variavel, mudando tambem visibilidade de construtor de public para protected, ja que é abtract e as filhas que vao usar

--- src/main/java/dev/davi/model/account/Conta.java
+++ src/main/java/dev/davi/model/account/Conta.java
@@ -6,16 +6,16 @@ import src.main.java.dev.davi.model.client.Cliente;
 public abstract class Conta implements IConta {
 	
 	private static final int AGENCIA_PADRAO = 1;
-	private static int SEQUENCIAL = 1;
+	private static int sequencial = 1;
 
 	protected int agencia;
 	protected int numero;
 	protected double saldo;
 	protected Cliente cliente;
 
-	public Conta(Cliente cliente) {
+	protected Conta(Cliente cliente) {
 		this.agencia = Conta.AGENCIA_PADRAO;
-		this.numero = SEQUENCIAL++;
+		this.numero = sequencial++;
 		this.cliente = cliente;
 	}
 
